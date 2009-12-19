90efb2be0481111019424c096bb05a88a8ab95ef
postmodern <postmodern.mod3@gmail.com>
2009-12-18 21:18:18 -0800

    Added ronin_rat_service.

---
+++ src/service.c
@@ -0,0 +1,47 @@
+#include <ronin/rat/service.h>
+#include <ronin/rat/func.h>
+
+#include <malloc.h>
+#include <string.h>
+
+ronin_rat_service_t * ronin_rat_service_create(const char *name)
+{
+	ronin_rat_service_t *new_service;
+
+	if (!(new_service = malloc(sizeof(ronin_rat_service_t))))
+	{
+		// malloc failed
+		goto cleanup;
+	}
+
+	if (!(new_service->name = strdup(name)))
+	{
+		goto cleanup_service;
+	}
+
+	if (!(new_service->functions = ronin_rat_list_create((ronin_rat_list_destroy_func)ronin_rat_func_destroy)))
+	{
+		goto cleanup_name;
+	}
+
+	return new_service;
+
+cleanup_name:
+	free(new_service->name);
+cleanup_service:
+	free(new_service);
+cleanup:
+	return NULL;
+}
+
+const ronin_rat_func_t * ronin_rat_service_search(const ronin_rat_service_t *service,const char *name)
+{
+	return (ronin_rat_func_t *)ronin_rat_list_search(service->functions,name);
+}
+
+void ronin_rat_service_destroy(ronin_rat_service_t *service)
+{
+	ronin_rat_list_destroy(service->functions);
+	free(service->name);
+	free(service);
+}

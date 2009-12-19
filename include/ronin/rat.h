f3c3b0d61f609aa8f77c15b06eee4d5d51b285a5
postmodern <postmodern.mod3@gmail.com>
2009-12-18 16:31:53 -0800

    Added ronin_rat_version.

---
+++ include/ronin/rat.h
@@ -0,0 +1,6 @@
+#ifndef _RONIN_RAT_H_
+#define _RONIN_RAT_H_
+
+const char * ronin_rat_version();
+
+#endif
---
+++ src/rat.c
@@ -0,0 +1,7 @@
+#include <ronin/rat.h>
+#include <ronin/rat/config.h>
+
+const char * ronin_rat_version()
+{
+	return RONIN_RAT_VERSION;
+}

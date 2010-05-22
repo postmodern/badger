f2ad64b6770fd6d1bea27efc2c2f07d4df961a1a
postmodern <postmodern.mod3@gmail.com>
2010-05-22 01:46:51 -0700

    Added mempool for controling and sanitizing allocated memory.

---
+++ include/badger/mempool.h
@@ -0,0 +1,29 @@
+#ifndef _MEMPOOL_H_
+#define _MEMPOOL_H_
+
+#include "slist.h"
+
+struct mempool_chunk
+{
+	size_t size;
+	void *ptr;
+};
+
+struct mempool_chunk * mempool_chunk_create(size_t size);
+void mempool_chunk_destroy(struct mempool_chunk *chunk);
+
+typedef struct slist mempool;
+
+static inline mempool * mempool_create()
+{
+	return slist_create(slist_compare_ints,(slist_destroy_func)mempool_chunk_destroy);
+}
+
+int mempool_alloc(mempool *mempool,size_t size);
+
+static inline void mempool_destroy(mempool *mempool)
+{
+	slist_destroy(mempool);
+}
+
+#endif
---
+++ src/mempool.c
@@ -0,0 +1,58 @@
+#include <badger/mempool.h>
+
+#include <malloc.h>
+#include <string.h>
+
+struct mempool_chunk * mempool_chunk_create(size_t size)
+{
+	void *new_ptr;
+
+	// allocate and zero the memory
+	if (!(new_ptr = calloc(size,1)))
+	{
+		return NULL;
+	}
+
+	struct mempool_chunk *new_chunk;
+
+	if (!(new_chunk = malloc(sizeof(struct mempool_chunk))))
+	{
+		free(new_ptr);
+		return NULL;
+	}
+
+	new_chunk->size = size;
+	new_chunk->ptr = new_ptr;
+	return new_chunk;
+}
+
+void mempool_chunk_destroy(struct mempool_chunk *chunk)
+{
+	// zero the memory before freeing it
+	memset(chunk->ptr,0,chunk->size);
+	free(chunk->ptr);
+
+	// zero the chunk fields before freeing it
+	chunk->ptr = NULL;
+	chunk->size = 0;
+
+	free(chunk);
+}
+
+int mempool_alloc(mempool *mempool,size_t size)
+{
+	struct mempool_chunk *new_chunk;
+
+	if (!(new_chunk = mempool_chunk_create(size)))
+	{
+		return -1;
+	}
+
+	if (slist_add(mempool,new_chunk->ptr,new_chunk) == -1)
+	{
+		mempool_chunk_destroy(new_chunk);
+		return -1;
+	}
+
+	return 0;
+}

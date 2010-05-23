5fd38a70ae47ff8eb29511835ea42235276f2c91
postmodern <postmodern.mod3@gmail.com>
2010-05-22 22:54:30 -0700

    Added a global mempool and badger_init / badger_fini functions.

--- include/badger/badger.h
+++ include/badger/badger.h
@@ -1,6 +1,29 @@
 #ifndef _BADGER_H_
 #define _BADGER_H_
 
-const char * badger_version();
+#include <badger/mempool.h>
+
+#include <assert.h>
+
+mempool_t *badger_global_mempool;
+
+extern const char * badger_version();
+
+extern void badger_init();
+extern void badger_fini();
+
+static void * badger_malloc(size_t size)
+{
+	assert(badger_global_mempool != NULL);
+
+	return mempool_alloc(badger_global_mempool,size);
+}
+
+static void badger_free(void *ptr)
+{
+	assert(badger_global_mempool != NULL);
+
+	mempool_free(badger_global_mempool,ptr);
+}
 
 #endif
--- src/badger.c
+++ src/badger.c
@@ -1,7 +1,25 @@
-#include <badger/badger.h>
 #include <badger/config.h>
+#include <badger/badger.h>
+#include <badger/mempool.h>
+
+#include <stdlib.h>
+#include <time.h>
+
+mempool_t *badger_global_mempool = NULL;
 
 const char * badger_version()
 {
 	return BADGER_VERSION;
+}
+
+void badger_init()
+{
+	srand(time(NULL));
+
+	badger_global_mempool = mempool_create();
+}
+
+void badger_fini()
+{
+	mempool_destroy(badger_global_mempool);
 }

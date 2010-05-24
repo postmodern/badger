ae03bbf4a42645fe39cf9052855524f7117db669
postmodern <postmodern.mod3@gmail.com>
2010-05-24 06:44:29 -0700

    Added more helper functions for working with a payload.

--- src/private/packet.c
+++ src/private/packet.c
@@ -1,16 +1,13 @@
 #include <badger/packet.h>
+#include "private/packet.h"
 #include "private/util.h"
-
-#include <malloc.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
 
 int badger_packet_valid(const unsigned char *packet,size_t packet_size)
 {
-	const void *packed_payload = (packet + 1 + sizeof(crc32_t));
-	size_t payload_size = (packet_size - 1 - sizeof(crc32_t));
+	const void *packed_payload = badger_packet_payload(packet);
+	size_t payload_size = (packet_size - BADGER_PACKET_HDRSIZE);
 
-	uint8_t packet_version = *packet;
+	uint8_t packet_version = badger_packet_version(packet);
 
 	if (packet_version != BADGER_PROTOCOL_VERSION)
 	{
@@ -18,7 +15,7 @@ int badger_packet_valid(const unsigned char *packet,size_t packet_size)
 		return 0;
 	}
 
-	crc32_t claimed_checksum = ntohl(*(crc32_t *)(packet + 1));
+	crc32_t claimed_checksum = badger_packet_checksum(packet);
 	crc32_t actual_checksum = badger_crc32(packed_payload,payload_size);
 
 	if (claimed_checksum != actual_checksum)
--- src/private/packet.h
+++ src/private/packet.h
@@ -1,7 +1,28 @@
 #ifndef _PRIVATE_BADGER_PACKET_H_
 #define _PRIVATE_BADGER_PACKET_H_
 
+#include <badger/packet.h>
+#include "private/util.h"
+
+#include <stdint.h>
 #include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+static inline uint8_t badger_packet_version(const unsigned char *packet)
+{
+	return *packet;
+}
+
+static inline crc32_t badger_packet_checksum(const unsigned char *packet)
+{
+	return ntohl(*(crc32_t *)(packet + 1));
+}
+
+static inline const unsigned char * badger_packet_payload(const unsigned char *packet)
+{
+	return packet + BADGER_PACKET_HDRSIZE;
+}
 
 int badger_packet_valid(const unsigned char *packet,size_t packet_size);
 

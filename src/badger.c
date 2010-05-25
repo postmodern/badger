d36d79f961565a0d5a84d951a4457d027f6e640f
postmodern <postmodern.mod3@gmail.com>
2010-05-24 19:12:09 -0700

    Removed mempool and badger_malloc/badger_free.

--- src/badger.c
+++ src/badger.c
@@ -1,11 +1,8 @@
 #include <badger/config.h>
 #include <badger/badger.h>
-#include <badger/mempool.h>
 
 #include <stdlib.h>
 #include <time.h>
-
-mempool_t *badger_global_mempool = NULL;
 
 const char * badger_version()
 {
@@ -15,13 +12,8 @@ const char * badger_version()
 void badger_init()
 {
 	srand(time(NULL));
-
-	badger_global_mempool = mempool_create();
 }
 
 void badger_fini()
 {
-	mempool_destroy(badger_global_mempool);
-
-	badger_global_mempool = NULL;
 }

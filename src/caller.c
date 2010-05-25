ffc0cdfc36d3b0643dcea23694e822fe948d1dc3
postmodern <postmodern.mod3@gmail.com>
2010-05-24 19:52:54 -0700

    Added checks to badger_yeild().

--- src/caller.c
+++ src/caller.c
@@ -4,8 +4,15 @@
 
 void badger_yield(badger_caller_t *caller)
 {
-	// zero the yield buffer before re-clearing it
-	memset(caller->yield_buffer.data,0,caller->yield_buffer.size);
-	msgpack_sbuffer_destroy(&(caller->yield_buffer));
-	msgpack_sbuffer_init(&(caller->yield_buffer));
+	if (caller->closure)
+	{
+		if (caller->yield_buffer.data)
+		{
+			// zero the yield buffer before re-clearing it
+			memset(caller->yield_buffer.data,0,caller->yield_buffer.size);
+		}
+
+		msgpack_sbuffer_destroy(&(caller->yield_buffer));
+		msgpack_sbuffer_init(&(caller->yield_buffer));
+	}
 }

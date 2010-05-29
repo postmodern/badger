efafd0b01e42ac437809485b5af0901e269c7915
postmodern <postmodern.mod3@gmail.com>
2010-05-28 23:42:24 -0700

    Updated the badger_crc32 function and table.

--- src/private/util.h
+++ src/private/util.h
@@ -13,8 +13,14 @@
 #define badger_debug(format,...)
 #endif
 
+/*
+ * Build auxiliary table for parallel byte-at-a-time CRC-32.
+ */
+#define CRC32_POLY 0x04c11db7     /* AUTODIN II, Ethernet, & FDDI */
+
 typedef uint32_t crc32_t;
 
+void badger_crc32_init();
 crc32_t badger_crc32(const void *data,size_t size);
 
 #endif

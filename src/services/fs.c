1b181b09fafeee2e07651cae32bf63638c079e05
postmodern <postmodern.mod3@gmail.com>
2010-05-30 05:53:10 -0700

    Added badger_fs_stat and badger_fs_unlink.

--- src/services/fs.c
+++ src/services/fs.c
@@ -8,11 +8,67 @@
 badger_service_t badger_fs_service = {
 	"fs",
 	{
+		&badger_fs_stat_func,
+		&badger_fs_unlink_func,
 		&badger_fs_open_func,
 		&badger_fs_close_func,
 		NULL
 	}
 };
+
+const badger_function_t badger_fs_stat_func = {"stat",badger_fs_close,1,{badger_data_string}};
+
+int badger_fs_stat(int argc,const badger_data_t *args,badger_caller_t *caller)
+{
+	size_t path_length = badger_string_length(args);
+	char path[path_length + 1];
+
+	memcpy(path,badger_string(args),path_length);
+	path[path_length] = '\0';
+
+	struct stat file;
+
+	if (stat(path,&file) == -1)
+	{
+		return BADGER_ERROR;
+	}
+
+	badger_return_array(caller,7);
+
+	// protection
+	badger_return_uint(caller,file.st_mode);
+	// user ID of owner
+	badger_return_uint(caller,file.st_uid);
+	// group ID of owner
+	badger_return_uint(caller,file.st_gid);
+	// total size, in bytes
+	badger_return_uint(caller,file.st_size);
+	// time of last access
+	badger_return_uint(caller,file.st_atime);
+	// time of last modification
+	badger_return_uint(caller,file.st_mtime);
+	// time of last status change
+	badger_return_uint(caller,file.st_ctime);
+	return BADGER_SUCCESS;
+}
+
+const badger_function_t badger_fs_unlink_func = {"unlink",badger_fs_unlink,1,{badger_data_string}};
+
+int badger_fs_unlink(int argc,const badger_data_t *args,badger_caller_t *caller)
+{
+	size_t path_length = badger_string_length(args);
+	char path[path_length + 1];
+
+	memcpy(path,badger_string(args),path_length);
+	path[path_length] = '\0';
+
+	if (unlink(path) == -1)
+	{
+		return BADGER_ERROR;
+	}
+
+	return BADGER_SUCCESS;
+}
 
 const badger_function_t badger_fs_open_func = {"open",badger_fs_open,1,{badger_data_string}};
 
@@ -40,24 +96,6 @@ const badger_function_t badger_fs_close_func = {"close",badger_fs_close,1,{badge
 int badger_fs_close(int argc,const badger_data_t *args,badger_caller_t *caller)
 {
 	if (close(badger_int(args) == -1))
-	{
-		return BADGER_ERROR;
-	}
-
-	return BADGER_SUCCESS;
-}
-
-const badger_function_t badger_fs_unlink_func = {"unlink",badger_fs_close,1,{badger_data_string}};
-
-int badger_fs_unlink(int argc,const badger_data_t *args,badger_caller_t *caller)
-{
-	size_t path_length = badger_string_length(args);
-	char path[path_length + 1];
-
-	memcpy(path,badger_string(args),path_length);
-	path[path_length] = '\0';
-
-	if (unlink(path) == -1)
 	{
 		return BADGER_ERROR;
 	}

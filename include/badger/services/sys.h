0cafee3319efd74acd956f85e0e9fab9c61bc9c9
postmodern <postmodern.mod3@gmail.com>
2010-05-29 19:31:08 -0700

    Added the beginnings of the Badger sys service.

---
+++ include/badger/services/sys.h
@@ -0,0 +1,8 @@
+#ifndef _BADGER_SERVICES_SYS_H_
+#define _BADGER_SERVICES_SYS_H_
+
+#include <badger/service.h>
+
+extern badger_service_t badger_sys_service;
+
+#endif
---
+++ src/services/sys.c
@@ -0,0 +1,8 @@
+#include <badger/services/sys.h>
+
+badger_service_t badger_sys_service = {
+	"sys",
+	{
+		{NULL,NULL,0}
+	}
+};

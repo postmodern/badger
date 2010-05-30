9ea2712416997acc599744b70e525e74282d15ae
postmodern <postmodern.mod3@gmail.com>
2010-05-29 22:06:12 -0700

    Added badger_sys_gethostname.

--- src/services/sys.c
+++ src/services/sys.c
@@ -9,6 +9,7 @@ badger_service_t badger_sys_service = {
 		&badger_sys_time_func,
 		&badger_sys_chdir_func,
 		&badger_sys_getcwd_func,
+		&badger_sys_gethostname_func,
 		NULL
 	}
 };
@@ -46,5 +47,20 @@ int badger_sys_getcwd(int argc,const badger_data_t *args,badger_caller_t *caller
 	}
 
 	badger_return_string(caller,cwd);
+	return BADGER_SUCCESS;
+}
+
+const badger_func_t badger_sys_gethostname_func = {"gethostname",badger_sys_gethostname,0};
+
+int badger_sys_gethostname(int argc,const badger_data_t *args,badger_caller_t *caller)
+{
+	char hostname[HOST_NAME_MAX];
+
+	if (gethostname(hostname,HOST_NAME_MAX) == -1)
+	{
+		return BADGER_ERROR;
+	}
+
+	badger_return_string(caller,hostname);
 	return BADGER_SUCCESS;
 }

f777c2c40ee2f43eb1ce1ac5aea9586a212c2106
postmodern <postmodern.mod3@gmail.com>
2010-05-29 20:10:49 -0700

    Added badger_sys_chdir.

--- include/badger/services/sys.h
+++ include/badger/services/sys.h
@@ -5,6 +5,10 @@
 
 extern badger_service_t badger_sys_service;
 
+extern const badger_func_t badger_sys_time_func;
 extern int badger_sys_time(int argc,const badger_data_t *args,badger_caller_t *caller);
+
+extern const badger_func_t badger_sys_chdir_func;
+extern int badger_sys_chdir(int argc,const badger_data_t *args,badger_caller_t *caller);
 
 #endif
--- src/services/sys.c
+++ src/services/sys.c
@@ -1,17 +1,29 @@
 #include <badger/services/sys.h>
 
 #include <time.h>
+#include <unistd.h>
 
 badger_service_t badger_sys_service = {
 	"sys",
 	{
-		{"time",badger_sys_time,0},
-		{NULL,NULL,0}
+		&badger_sys_time_func,
+		&badger_sys_chdir_func,
+		NULL
 	}
 };
+
+const badger_func_t badger_sys_time_func = {"time",badger_sys_time,0};
 
 int badger_sys_time(int argc,const badger_data_t *args,badger_caller_t *caller)
 {
 	badger_return_uint(caller,time(NULL));
+	return 0;
+}
+
+const badger_func_t badger_sys_chdir_func = {"chdir",badger_sys_chdir,1,{badger_data_string}};
+
+int badger_sys_chdir(int argc,const badger_data_t *args,badger_caller_t *caller)
+{
+	chdir(badger_string(args));
 	return 0;
 }

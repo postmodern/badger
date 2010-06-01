dc2fe6674c5256f40a962fc0c5864642ba976cfd
postmodern <postmodern.mod3@gmail.com>
2010-05-31 20:50:36 -0700

    How did this not get added?

---
+++ include/badger/services/ffi.h
@@ -0,0 +1,26 @@
+#ifndef _BADGER_SERVICES_FFI_H_
+#define _BADGER_SERVICES_FFI_H_
+
+#include <badger/service.h>
+
+extern const badger_service_t badger_ffi_service;
+
+extern const badger_function_t badger_ffi_open_func;
+extern int badger_ffi_open(int argc,const badger_data_t *args,badger_caller_t *caller);
+
+extern const badger_function_t badger_ffi_libraries_func;
+extern int badger_ffi_libraries(int argc,const badger_data_t *args,badger_caller_t *caller);
+
+extern const badger_function_t badger_ffi_attach_function_func;
+extern int badger_ffi_attach_function(int argc,const badger_data_t *args,badger_caller_t *caller);
+
+extern const badger_function_t badger_ffi_exposed_functions_func;
+extern int badger_ffi_exposed_functions(int argc,const badger_data_t *args,badger_caller_t *caller);
+
+extern const badger_function_t badger_ffi_exposed_function_func;
+extern int badger_ffi_exposed_function(int argc,const badger_data_t *args,badger_caller_t *caller);
+
+extern const badger_function_t badger_ffi_close_func;
+extern int badger_ffi_close(int argc,const badger_data_t *args,badger_caller_t *caller);
+
+#endif

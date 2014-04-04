82bae7837c2026014646469f962c4b77d9f45e29
Postmodern <postmodern.mod3@gmail.com>
2014-04-04 16:04:33 -0700

    Added super basic option parsing.

--- bin/badger.rb
+++ bin/badger.rb
@@ -1,1 +1,16 @@
-puts "badger #{Badger::VERSION}"
+def usage
+  puts "usage: badger [OPTIONS]"
+end
+
+case ARGV[0]
+when '-V', '--version'
+  puts "badger #{Badger::VERSION}"
+when '-h', '--help'
+  usage
+when nil
+  $stderr.puts "badger: no options specified"
+  exit -1
+else
+  $stderr.puts "badger: unknown option: #{ARGV[0]}"
+  exit -1
+end

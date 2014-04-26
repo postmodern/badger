d5bdfc4624cdfaa5453b806e1ffc433f7320b834
Postmodern <postmodern.mod3@gmail.com>
2014-04-25 17:09:10 -0700

    Added Message::Responses and extra validations to Message.parse.

--- lib/badger/message.rb
+++ lib/badger/message.rb
@@ -3,6 +3,34 @@ module Badger
     class InvalidMessage < RuntimeError
     end
 
+    module Responses
+      #
+      # Returns a return response message.
+      #
+      # @param [Object] value
+      #   The return value.
+      #
+      # @return [Hash]
+      #   The response message.
+      #
+      def self.return(value)
+        {'return' => value}
+      end
+
+      #
+      # Returns an exception response message.
+      #
+      # @param [Exception] error
+      #   The exception.
+      #
+      # @return [Hash]
+      #   The response message.
+      #
+      def self.exception(error)
+        {'exception' => error.message}
+      end
+    end
+
     #
     # Encodes the message.
     #
@@ -25,12 +53,34 @@ module Badger
     # @return [Hash]
     #   The decoded message.
     #
-    def self.decode(message)
-      begin
-        JSON.parse(message.chomp)
-      rescue JSON::ParserError => error
-        raise(InvalidMessage,error.message,error.backtrace)
+    def self.decode(data)
+      message = begin
+                  JSON.parse(data.chomp)
+                rescue JSON::ParserError => error
+                  raise(InvalidMessage,error.message,error.backtrace)
+                end
+
+      unless message.kind_of?(Hash)
+        raise(InvalidMessage,"message was not a Hash")
       end
+
+      unless message.has_key?('name')
+        raise(InvalidMessage,"message was missing the 'name' field")
+      end
+
+      unless message['name'].kind_of?(String)
+        raise(InvalidMessage,"'name' was not a String")
+      end
+
+      unless message.has_key?('arguments')
+        raise(InvalidMessage,"message was missing the 'arguments' field")
+      end
+
+      unless message['arguments'].kind_of?(Array)
+        raise(InvalidMessage,"'arguments' was not an Array")
+      end
+
+      return message
     end
   end
 end

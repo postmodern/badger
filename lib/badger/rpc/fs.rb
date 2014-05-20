8e9dc6322c33e52e7c37b83c0f28f1cc0dc8cac7
liquid <liquid@localhost.localdomain>
2014-05-20 15:31:26 -0400

    changed the functions to work with fileno kept in an array:wq

--- lib/badger/rpc/fs.rb
+++ lib/badger/rpc/fs.rb
@@ -1,40 +1,44 @@
  module Badger
   module RPC
     module Fs
-      def self.open(path,mode); File.new(path,mode).fileno; end
+      @files = Array.new
+      def self.open(path,mode)
+        fd = File.new(path,mode)
+        @files[fd.fileno] = fd
+        return fd.fileno
+      end
 
-        def self.read(fd,position)
-          file = File.for_fd(fd)
-          file.seek(position)
+      def self.read(fileno,position)
+        file = File.for_fd(@files[fileno])
+        file.seek(position)
 
-          return (file.read(BLOCK_SIZE) || '')
-        end
+        return (file.read(2000) || '')
+      end
 
-          def self.write(fd,position,data)
-            file = File.for_fd(fd)
-            file.seek(position)
+      def self.write(fileno,position,data)
+        file = File.for_fd(@files[fileno])
+        file.seek(position)
 
-            return file.write(data)
-          end
+        return file.write(data)
+      end
 
-          def self.seek(fd,position)
-            file = File.for_fd(fd)
-            file.seek(position)
+      def self.seek(fileno,position)
+        file = File.for_fd(@files[fileno])
+        file.seek(position)
 
-            return file.pos
-          end
+        return file.pos
+      end
 
-          def self.close(fd); file = File.for_fd(fd).close; end
+      def self.close(fileno); file = File.for_fd(@files[fileno]).close; end
 
-          def self.readlink(path);           File.readlink(path);                end
-          def self.mktemp(basename);         Tempfile.new(basename).path;        end
-          def self.unlink(path);             File.unlink(path);                  end
-          def self.new(name,mode)            File.new(name,mode)                 end
-          def self.delete(name)              File.delete(name)                   end
-          def self.realpath(path)            File.realpath(path)                 end
+      def self.readlink(path);           File.readlink(path);                end
+      def self.mktemp(basename);         Tempfile.new(basename).path;        end
+      def self.unlink(path);             File.unlink(path);                  end
+      def self.new(name,mode)            File.new(name,mode)                 end
+      def self.delete(name)              File.delete(name)                   end
+      def self.realpath(path)            File.realpath(path)                 end
 
-          def self.stat(path);               File.stat(path);                    end
-          def self.compare(path,other_path); File.compare_file(path,other_path); end
+      def self.stat(path);               File.stat(path);                    end
   end
 end
 end
